9d19fd080b925d17980381e268edf0bdaa0bf179
boev <iordan.boev@gmail.com>
2014-01-22 13:36:52 +0100

    Small test change.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-jobmail
+job-direct-mail-plugin
 =======
 
 Jenkins plugin for sending mails from job or build view.
--- src/test/java/org/jenkinsci/plugins/JobDirectMailTest.java
+++ src/test/java/org/jenkinsci/plugins/JobDirectMailTest.java
@@ -22,8 +22,11 @@ import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.recipes.LocalData;
 
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlCheckBoxInput;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlSelect;
 import com.gargoylesoftware.htmlunit.html.HtmlTextArea;
@@ -32,7 +35,7 @@ import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 public class JobDirectMailTest {
     @Rule
     public JenkinsRule jenkinsRule = new JenkinsRule();
-    
+
     /**
      * Tests the global configuration class.
      * 
@@ -42,7 +45,6 @@ public class JobDirectMailTest {
     @Test
     public void testGlobalConfig() throws Exception {
         final Template t = getNewTemplate();
-        // final HtmlPage page = new WebClient().goTo("configure");
         assertNotNull(t);
         assertEquals(t.getName(), "SampleName");
         assertEquals(t.getText(), "SampleTextBlaBlaBla");
@@ -70,17 +72,20 @@ public class JobDirectMailTest {
         final Logger LOGGER = Logger.getLogger(JobMailProjectAction.class
                 .getName());
         checkIfJobsAreLoaded();
-        // addTemplates();
+        //addTemplates();
         testBaseAction();
 
-        final HtmlPage page = jenkinsRule.createWebClient().goTo("job/test_job/send_mail");
+        final HtmlPage page = jenkinsRule.createWebClient().goTo(
+                "job/test_job/send_mail");
 
         checkElementsAsStrings(page.asText());
 
         final HtmlForm form = populateForm(page);
 
         final Template t = getNewTemplate();
-        JobMailProjectAction a = new JobMailProjectAction( (AbstractProject<?, ?>) Jenkins.getInstance().getAllItems().get(0));
+        JobMailProjectAction a = new JobMailProjectAction(
+                (AbstractProject<?, ?>) Jenkins.getInstance().getAllItems()
+                        .get(0));
         assertNotNull(a.getTemplateText(t));
         try {
             assertNotNull(a.getDefaultSubject());
@@ -108,16 +113,18 @@ public class JobDirectMailTest {
     public void testBuildAction() throws IOException, SAXException {
 
         checkIfJobsAreLoaded();
-        // addTemplates();
-        final HtmlPage page = jenkinsRule.createWebClient().goTo("job/test_job/4/send_mail");
+        addTemplates();
+        final HtmlPage page = jenkinsRule.createWebClient().goTo(
+                "job/test_job/4/send_mail");
 
         checkElementsAsStrings(page.asText());
 
         final HtmlForm form = populateForm(page);
 
         final Template t = getNewTemplate();
-        JobMailBuildAction a = new JobMailBuildAction(((AbstractProject<?, ?>) Jenkins.getInstance()
-                .getAllItems().get(0)).getLastBuild());
+        JobMailBuildAction a = new JobMailBuildAction(
+                ((AbstractProject<?, ?>) Jenkins.getInstance().getAllItems()
+                        .get(0)).getLastBuild());
         assertNotNull(a.getTemplateText(t));
         try {
             assertNotNull(a.getDefaultSubject());
@@ -147,41 +154,40 @@ public class JobDirectMailTest {
     // testBuildAction(org.jenkinsci.plugins.Test): 404 Not Found for
     // http://localhost:58693/plugin/email-ext/scripts/emailext-behavior.js
 
-    // private void addTemplates() throws IOException, SAXException {
-    // final HtmlPage page = new WebClient().goTo("configure");
-    // final String allElements = page.asText();
-    // assertTrue(allElements.contains("Send Mail from job or build view"));
-    // assertTrue(allElements.contains("Show Templates and Options"));
-    // HtmlForm globalConfigForm = page.getFormByName("config");
-    // assertNotNull("GlobalConfigForm is null!", globalConfigForm);
-    // final HtmlInput ob = globalConfigForm.getInputByName("optionBlock");
-    // assertNotNull("OptionBlock is null!", ob);
-    // ob.setValueAttribute("true");
-    // final HtmlElement templates = (HtmlElement) globalConfigForm
-    // .getByXPath("//tr[td='Text Templates']").get(0);
-    // assertNotNull("Templates list is null!", templates);
-    // assertNotNull("Add button not found for templates",
-    // templates.getFirstByXPath(".//button"));
-    // ((HtmlButton) templates.getFirstByXPath(".//button")).click();
-    //
-    // final HtmlTextInput template1Name = (HtmlTextInput) templates
-    // .getFirstByXPath("//input[@name='" + "templates.name" + "']");
-    // assertNotNull("template1Name is null!", template1Name);
-    // template1Name.setValueAttribute("TestTemplate");
-    //
-    // final HtmlTextInput template1Text = (HtmlTextInput) templates
-    // .getFirstByXPath("//input[@name='" + "templates.text" + "']");
-    // assertNotNull("template1Text is null!", template1Text);
-    // template1Text.setValueAttribute("Some RrRRRrandom text.");
-    //
-    // final HtmlTextInput template1AddProjectname = (HtmlTextInput)
-    // templates
-    // .getFirstByXPath("//input[@name='" + "templates.addProjectName"
-    // + "']");
-    // assertNotNull("template1AddProjectname is null!",
-    // template1AddProjectname);
-    // template1AddProjectname.setValueAttribute("true");
-    // }
+    private void addTemplates() throws IOException, SAXException {
+        final HtmlPage page = jenkinsRule.createWebClient().goTo("configure");
+        final String allElements = page.asText();
+        assertTrue(allElements.contains("Send Mail from job or build view"));
+        assertTrue(allElements.contains("Show Templates and Options"));
+        HtmlForm globalConfigForm = page.getFormByName("config");
+        assertNotNull("GlobalConfigForm is null!", globalConfigForm);
+        final HtmlInput ob = globalConfigForm.getInputByName("optionBlock");
+        assertNotNull("OptionBlock is null!", ob);
+        ob.setValueAttribute("true");
+        final HtmlElement templates = (HtmlElement) globalConfigForm
+                .getByXPath("//tr[td='Text Templates']").get(0);
+        assertNotNull("Templates list is null!", templates);
+        assertNotNull("Add button not found for templates",
+                templates.getFirstByXPath(".//button"));
+        ((HtmlElement) templates.getFirstByXPath(".//button")).click();
+
+        final HtmlTextInput template1Name = (HtmlTextInput) templates
+                .getFirstByXPath("//input[@name='" + "templates.name" + "']");
+        assertNotNull("template1Name is null!", template1Name);
+        template1Name.setValueAttribute("TestTemplate");
+
+        final HtmlTextInput template1Text = (HtmlTextInput) templates
+                .getFirstByXPath("//input[@name='" + "templates.text" + "']");
+        assertNotNull("template1Text is null!", template1Text);
+        template1Text.setValueAttribute("Some RrRRRrandom text.");
+
+        final HtmlTextInput template1AddProjectname = (HtmlTextInput) templates
+                .getFirstByXPath("//input[@name='" + "templates.addProjectName"
+                        + "']");
+        assertNotNull("template1AddProjectname is null!",
+                template1AddProjectname);
+        template1AddProjectname.setValueAttribute("true");
+    }
 
     private HtmlForm populateForm(final HtmlPage page) {
         final HtmlForm form = page.getFormByName("mailForm");
@@ -214,12 +220,12 @@ public class JobDirectMailTest {
     }
 
     private void checkIfJobsAreLoaded() {
-        assertNotNull("job missing.. @LocalData problem?", Jenkins.getInstance()
-                .getItem("test_job"));
-        assertNotNull("job missing.. @LocalData problem?", Jenkins.getInstance()
-                .getItem("success"));
-        assertNotNull("job missing.. @LocalData problem?", Jenkins.getInstance()
-                .getItem("no_change"));
+        assertNotNull("job missing.. @LocalData problem?", Jenkins
+                .getInstance().getItem("test_job"));
+        assertNotNull("job missing.. @LocalData problem?", Jenkins
+                .getInstance().getItem("success"));
+        assertNotNull("job missing.. @LocalData problem?", Jenkins
+                .getInstance().getItem("no_change"));
     }
 
     /**
